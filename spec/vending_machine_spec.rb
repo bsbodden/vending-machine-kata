05830d8eb224b0d92a2f9916dc2e6282ae79ef62
Brian Sam-Bodden <bsbodden@integrallis.com>
2017-02-13 11:46:27 -0700

    Specs Refactor: Move #insert specs into their own group

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,52 +1,55 @@
 require 'spec_helper'
 require_relative '../src/vending_machine'
 
-describe VendingMachine, '#insert' do
-  before(:each) do
-    @vending_machine = VendingMachine.new
-  end
+describe VendingMachine do
 
-  context 'When inserting a nickle,' do
-    it 'is accepted' do
-      expect(@vending_machine.insert(:nickle)).to be(:ok)
+  describe '#insert' do
+    before(:each) do
+      @vending_machine = VendingMachine.new
     end
 
-    it 'is added to the coins in the current transaction' do
-      @vending_machine.insert(:nickle)
-      expect(@vending_machine.coins).to contain_exactly(:nickle)
-    end
-  end
+    context 'When inserting a nickle,' do
+      it 'is accepted' do
+        expect(@vending_machine.insert(:nickle)).to be(:ok)
+      end
 
-  context 'When inserting a dime,' do
-    it 'is accepted' do
-      expect(@vending_machine.insert(:dime)).to be(:ok)
-    end
-
-    it 'is added to the coins in the current transaction' do
-      @vending_machine.insert(:dime)
-      expect(@vending_machine.coins).to contain_exactly(:dime)
-    end
-  end
-
-  context 'When inserting a quarter,' do
-    it 'is accepted' do
-      expect(@vending_machine.insert(:quarter)).to be(:ok)
+      it 'is added to the coins in the current transaction' do
+        @vending_machine.insert(:nickle)
+        expect(@vending_machine.coins).to contain_exactly(:nickle)
+      end
     end
 
-    it 'is added to the coins in the current transaction' do
-      @vending_machine.insert(:quarter)
-      expect(@vending_machine.coins).to contain_exactly(:quarter)
-    end
-  end
+    context 'When inserting a dime,' do
+      it 'is accepted' do
+        expect(@vending_machine.insert(:dime)).to be(:ok)
+      end
 
-  context 'When inserting a penny,' do
-    it 'is rejected' do
-      expect(@vending_machine.insert(:penny)).to be(:rejected)
+      it 'is added to the coins in the current transaction' do
+        @vending_machine.insert(:dime)
+        expect(@vending_machine.coins).to contain_exactly(:dime)
+      end
     end
 
-    it 'is NOT added to the coins in the current transaction' do
-      @vending_machine.insert(:penny)
-      expect(@vending_machine.coins).to be_empty
+    context 'When inserting a quarter,' do
+      it 'is accepted' do
+        expect(@vending_machine.insert(:quarter)).to be(:ok)
+      end
+
+      it 'is added to the coins in the current transaction' do
+        @vending_machine.insert(:quarter)
+        expect(@vending_machine.coins).to contain_exactly(:quarter)
+      end
+    end
+
+    context 'When inserting a penny,' do
+      it 'is rejected' do
+        expect(@vending_machine.insert(:penny)).to be(:rejected)
+      end
+
+      it 'is NOT added to the coins in the current transaction' do
+        @vending_machine.insert(:penny)
+        expect(@vending_machine.coins).to be_empty
+      end
     end
   end
 end
