504b3d6249923917cddec3bd3bda68b552b05895
Brian Sam-Bodden <bsbodden@integrallis.com>
2017-02-13 11:20:00 -0700

    Refactor: move vending machine initialization to before block

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -2,34 +2,30 @@ require 'spec_helper'
 require_relative '../src/vending_machine'
 
 describe VendingMachine, '#insert' do
+  before(:each) do
+    @vending_machine = VendingMachine.new
+  end
+
   context 'When inserting a nickle,' do
     it 'is accepted' do
-      @vending_machine = VendingMachine.new
-
       expect(@vending_machine.insert(:nickle)).to be(:ok)
     end
   end
 
   context 'When inserting a dime,' do
     it 'is accepted' do
-      @vending_machine = VendingMachine.new
-
       expect(@vending_machine.insert(:dime)).to be(:ok)
     end
   end
 
   context 'When inserting a quarter,' do
     it 'is accepted' do
-      @vending_machine = VendingMachine.new
-
       expect(@vending_machine.insert(:quarter)).to be(:ok)
     end
   end
 
   context 'When inserting a penny,' do
     it 'is rejected' do
-      @vending_machine = VendingMachine.new
-
       expect(@vending_machine.insert(:penny)).to be(:rejected)
     end
   end
