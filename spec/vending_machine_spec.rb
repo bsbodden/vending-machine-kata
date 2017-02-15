038c81b59df9bbb67b1dc849d32721f72bcee66a
Brian Sam-Bodden <bsbodden@integrallis.com>
2017-02-14 20:17:44 -0700

    Use ALLOWED_PRODUCTS to PRODUCTS for consistency

--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -199,7 +199,7 @@ describe VendingMachine do
   end
 
   # There are three products: cola for $1.00, chips for $0.50, and candy for $0.65.
-  describe 'ALLOWED_PRODUCTS' do
+  describe 'PRODUCTS' do
     it 'returns three (3) products' do
       expect(VendingMachine::PRODUCTS.keys).to contain_exactly(:cola, :chips, :candy)
     end
